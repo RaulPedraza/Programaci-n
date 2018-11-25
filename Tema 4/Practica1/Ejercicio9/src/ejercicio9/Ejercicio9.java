894b5e45af7e416cca7614aa202841a43bbb2312
Unknown <raul.pedraza@ikasle.egibide.org>
2018-11-25 11:41:16 +0100

    Ejercicio9

---
+++ Tema 4/Practica1/Ejercicio9/src/ejercicio9/Ejercicio9.java
@@ -0,0 +1,32 @@
+
+package ejercicio9;
+
+import javax.swing.JOptionPane;
+import java.util.Arrays;
+
+public class Ejercicio9 {
+
+    
+    public static void main(String[] args) {
+        int [][] matriz = new  int [3][4];
+        /*En este ejemplo tendriamos este numero de alumnos
+        int[][]  alumnos = {{3,5,16,6},{5,13,7,9},{2,6,15,18}};
+        */
+        //NIVEL BASICO
+        matriz[0][0] = 3;
+        matriz[0][1] = 5;
+        matriz[0][2] = 16;
+        matriz[0][3] = 6;
+        //NIVEL MEDIO
+        matriz[1][0] = 5;
+        matriz[1][1] = 13;
+        matriz[1][2] = 7;
+        matriz[1][3] = 9;
+        //NIVEL DE PERFECCIONAMIENTO
+        matriz[2][0] = 2;
+        matriz[2][1] = 6;
+        matriz[2][2] = 15;
+        matriz[2][3] = 18;
+            
+    }   
+}
