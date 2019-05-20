388e005bc6824f569f2135bd4f25c39ca3ca287b
Raulpedraza <raul.pedraza@ikasle.egibide.org>
2019-05-20 14:06:52 +0200

    ventanas

---
+++ Repaso/Ejercicio19/src/ejercicio19/Ejercicio19.java
@@ -0,0 +1,94 @@
+package ejercicio19;
+
+import Clases.Persona;
+import Ventanas.VentanaDatos;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+
+public class Ejercicio19 {
+
+    private static Persona p;
+    private static ArrayList<Persona> personas;
+    private static VentanaDatos vd;
+    private static int continuar = 0;
+    
+    public static void main(String[] args) {
+        personas = new ArrayList();
+        //Abrir Ventana
+        vd = new VentanaDatos();
+        vd.setVisible(true);
+
+    }
+    
+    public static void obtenerDatos(String dni,String n,String ap,String s, String ed,String pe){
+        p.setDni(dni);
+        p.setNombre(n);
+        p.setApellidos(ap);
+        p.setSexo(s);
+        int edad = Integer.parseInt(ed);
+        p.setEdad(edad);
+        int peso = Integer.parseInt(pe);
+        p.setPeso(peso);
+
+        personas.add(p);
+    }
+    
+    public static void validarDatos(){
+        
+    }
+    
+    public static void menu(){
+        int opcion = Integer.parseInt(JOptionPane.showInputDialog("Elige una opcion: \n"
+                + "1.-Buscar una persona con el DNI \n"
+                + "2.-Eliminar una persona \n"
+                + "3.-Ordenar por apellido \n"
+                + "4.-Mostrar todas las personas"));
+        switch(opcion){
+            case 1:
+                buscar();
+                break;
+            case 2: 
+                eliminar();
+                break;
+            case 3:
+                ordenar();
+                break;
+            case 4:
+                mostrar();
+                break;
+        }
+    }
+    public static void buscar(){
+        String dni = JOptionPane.showInputDialog("Introduce el DNI de la persona que quieres buscar");
+        for(int x = 0; x < personas.size(); x++){
+            if(personas.get(x).getDni().equals(dni)){
+                JOptionPane.showMessageDialog(null,"DNI: " + personas.get(x).getDni() + "\n"
+                + "Apellido: " + personas.get(x).getApellidos() + "\n"
+                + "Nombre: " + personas.get(x).getNombre() + "\n"
+                + "Sexo: " + personas.get(x).getSexo() + "\n"
+                + "Edad: " + personas.get(x).getEdad() + "\n"
+                + "Peso " + personas.get(x).getPeso());
+            }else
+                JOptionPane.showMessageDialog(null, "Ese DNI no corresponde a ninguna persona");
+        }
+    }
+    
+    public static void eliminar(){
+        String dni = JOptionPane.showInputDialog("Introduce el DNI de la persona que quieres eliminar");
+        for(int x = 0; x < personas.size(); x++){
+            if(personas.get(x).getDni().equals(dni)){
+                personas.remove(x);
+            }else
+                JOptionPane.showMessageDialog(null, "Ese DNI no corresponde a ninguna persona");
+        }    
+    }
+    
+    public static void ordenar(){
+        
+    }
+    
+    public static void mostrar(){
+        
+    }
+}
